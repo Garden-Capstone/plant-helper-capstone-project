c4cea53a787f1b15f27843a5dbd647fc62aad5ac
Leah Abraham <leahabraham1111@gmail.com>
2024-08-15 14:54:54 -0600

    made develop branch

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,29 @@
+"use client"
+import { Navbar } from "flowbite-react"
+
+import Link from "next/link";
 export default function Home() {
     return (
-        <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
-    )
+
+            <Navbar fluid rounded>
+                <Navbar.Brand as={Link} href="https://flowbite-react.com">
+
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Navbar.Link href="#" active>
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link as={Link} href="#">
+                        About
+                    </Navbar.Link>
+                    <Navbar.Link href="#">Services</Navbar.Link>
+                    <Navbar.Link href="#">Pricing</Navbar.Link>
+                    <Navbar.Link href="#">Contact</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+
+)
 }
+
