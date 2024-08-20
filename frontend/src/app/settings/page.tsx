ae17698ff3d1d27f2fea0a8c30110d40a9e5c181
ashley medrano <amedrano19@cnm.edu>
2024-08-19 22:07:15 -0600

    saved changes to settings page

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import { Button, Navbar } from "flowbite-react";
+import { Label, TextInput } from "flowbite-react";
+import Link from "next/link";
 
 
 export default function Settings() {
@@ -12,16 +14,17 @@ export default function Settings() {
 
     return (
         <>
-            {/*Navbar*/}
-            <Navbar fluid rounded className="justify-between">
+        {/*Navbar*/}
+        <Navbar fluid rounded className="justify-between">
             <Navbar.Brand className="relative">
-                <a href="#">
-                    <svg className="w-10 h-10 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                <Link href="/">
+                    <svg className="w-9 h-9 text-gray-800 "
+                         aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="m15 19-7-7 7-7"/>
                     </svg>
-                </a>
+                </Link>
             </Navbar.Brand>
 
             <div className="flex-1 flex items-center mr-10 justify-center">
@@ -33,28 +36,79 @@ export default function Settings() {
             </div>
         </Navbar>
 
-            {/* Profile Picture Section */}
-            <div className="flex flex-col items-center justify-center">
-              <div className="flex flex-col items-center mx-16 my-16">
-                 <img
+        {/* Profile Picture Section */}
+        <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center mx-14 my-10">
+                <img
                     src={profilePic}
                     alt="Profile"
                     className="w-40 h-40 rounded-full border-2 border-gray-300"
-                 />
+                />
 
-                  <div>
-                      <label htmlFor="file-input">
-                          <Button
-                              className="text-sm mt-12"
-                              aria-label="Change Profile Picture">Change Picture
-                          </Button>
-                      </label>
-                  </div>
-              </div>
+                <div>
+                    <label htmlFor="file-input">
+                        <Button
+                            className="text-sm mt-12"
+                            aria-label="Change Profile Picture">Change Picture
+                        </Button>
+                    </label>
+                </div>
+            </div>
+        </div>
+
+
+        {/* Form Section */}
+        <div className="flex max-w-xl flex-col mx-auto gap-7">
+            <div>
+                <div className="mb-2 block">
+                    <Label
+                        htmlFor="small"
+                        className="text-md"
+                        value="First Name"/>
+                </div>
+                <textarea
+                    id="small"
+                    className="p-2 border rounded-lg w-full h-11 bg-gray-50 border-gray-300 resize-none overflow-hidden"
+                    placeholder="Enter first name here.."/>
             </div>
 
+            <div>
+                <div className="mb-2 block">
+                    <Label
+                        htmlFor="small"
+                        className="text-md"
+                        value="Last Name"/>
+                </div>
+                <textarea
+                    id="small"
+                    className="p-2 border rounded-lg w-full h-11 bg-gray-50 border-gray-300 resize-none overflow-hidden"
+                    placeholder="Enter last name here.."/>
+            </div>
+
+            <div>
+                <div className="mb-2 block">
+                    <Label
+                        htmlFor="large"
+                        className="text-md"
+                        value="What are your plant goals?"/>
+                </div>
+                <textarea
+                    id="large"
+                    className="p-4 mb-4 rounded-lg border border-gray-300 w-full h-32 bg-gray-50 resize-none overflow-hidden"
+                    placeholder="Enter your goals here..."/>
+            </div>
+        </div>
+
+            {/* Save Changes Button */}
+        <div className="flex flex-col items-center justify-center">
+            <label htmlFor="file-input">
+                <Button
+                    className="text-sm flex flex-col items-center mx-14 my-10"
+                    aria-label="Change Profile Picture">Save Changes
+                </Button>
+            </label>
+        </div>
 
         </>
-
-);
+    );
 }
