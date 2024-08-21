9c8ded6cf2a8c152377273a6c78cecd86d9a16b2
ashley medrano <amedrano19@cnm.edu>
2024-08-20 21:11:36 -0600

    added changes to login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,44 +4,47 @@ import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react"
 export default function Login() {
     return (
         <>
+            {/* Background Image */}
             <div
-                className="absolute inset-0 bg-local"
+                className="fixed inset-0 bg-local top-0 left-0 right-0"
                 style={{
-                    backgroundImage: "url('https://img.freepik.com/free-vector/beige-leafy-watercolor-background-vector_53876-136491.jpg?w=1060&t=st=1724022751~exp=1724023351~hmac=f224ba527076b4131766c1125d7785597015d37c9d3d7379648cb1d0f1030137')",
+                    backgroundImage: "url('https://images.unsplash.com/photo-1523575708161-ad0fc2a9b951?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     zIndex: '-1'
                 }}
             ></div>
 
-            <Card className="relative max-w-lg mx-auto my-40 bg-white bg-opacity-80 shadow-2xl">
+            <Card className="flex max-w-lg mx-auto mt-20 overflow-hidden bg-white bg-opacity-90 shadow-2xl">
                 <div>
-                    <p className="flex justify-center pt-16 text-4xl">Plant Whisperer</p>
+                    <p className="flex justify-center pt-16 text-[#2E8B57] text-4xl">Plant Whisperer</p>
                 </div>
                 <div>
-                    <p className="flex justify-center pb-14">Blah Blah Blah</p>
+                    <p className="flex justify-center pb-14 text-[#3CB371]">Blah Blah Blah</p>
                 </div>
                 <form className="flex flex-col gap-4 items-center">
                     <div>
                         <div className="mb-2 block">
-                            <Label htmlFor="email1" value="Email" />
+                            <Label htmlFor="email1" value="Email" className="text-[#2E8B57]"/>
                         </div>
-                        <TextInput className="w-96" id="email1" type="email" placeholder="email" required />
+                        <TextInput className="w-96" id="email1" type="email" placeholder="email" required/>
                     </div>
                     <div>
                         <div className="mb-2 block">
-                            <Label htmlFor="password1" value="Password" />
+                            <Label htmlFor="password1" value="Password" className="text-[#2E8B57]"/>
                         </div>
-                        <TextInput className="w-96" id="password1" type="password" placeholder="password" required />
+                        <TextInput className="w-96" id="password1" type="password" placeholder="password" required/>
                     </div>
-                    <Button className="w-96 mt-10 py-2" type="submit">
+                    <Button className="w-60 mt-10 py-2 bg-[#2E8B57] enabled:hover:bg-[#2c6e49]" type="submit">
                         Sprout Back In!
                     </Button>
                     <div className="mt-7 mb-8 text-xs">
-                        <p>Don't have an account? Sign up to start growing today!</p>
+                        <span>Don't have an account? </span>
+                        <span className="text-[#FF9999]">Sign up </span>
+                        <span>to start growing today!</span>
                     </div>
                 </form>
             </Card>
         </>
-    );
+    )
 }
