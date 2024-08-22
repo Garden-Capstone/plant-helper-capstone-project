9341c67f9fe84b0a2bb48da05fa4839efbf2f6a9
ashley medrano <amedrano19@cnm.edu>
2024-08-21 21:37:29 -0600

    put finishing touches on settings page

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -16,13 +16,15 @@ export default function Settings() {
     const pictures = [
         "https://images.unsplash.com/photo-1516048015710-7a3b4c86be43?q=80&w=1625&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
 
+        "https://images.unsplash.com/photo-1509937528035-ad76254b0356?q=80&w=1624&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
+
         "https://images.unsplash.com/photo-1459411552884-841db9b3cc2a?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
 
-        "https://images.unsplash.com/photo-1611866759729-0cba525f9b45?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
+        "https://images.unsplash.com/photo-1485955900006-10f4d324d411?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
 
         "https://images.unsplash.com/photo-1504280335916-c3e24a215b8b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
 
-        "https://images.unsplash.com/photo-1485955900006-10f4d324d411?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
+        "https://images.unsplash.com/photo-1611866759729-0cba525f9b45?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/100",
     ];
 
     {/*This code is for the picture selection*/}
@@ -33,44 +35,44 @@ export default function Settings() {
 
     return (
         <>
-        {/*Navbar*/}
-        <Navbar fluid rounded className="justify-between bg-[#2c6e49]">
-            <Navbar.Brand className="relative">
-                <Link href="/">
-                    <svg
-                       className="w-9 h-9 text-[#f9f7ef]"
-                       aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg"
-                       width="24"
-                       height="24"
-                       fill="none"
-                       viewBox="0 0 24 24">
-                       <path
-                       stroke="currentColor"
-                       strokeLinecap="round"
-                       strokeLinejoin="round"
-                       strokeWidth="2"
-                       d="m15 19-7-7 7-7"/>
-                       </svg>
-                </Link>
-            </Navbar.Brand>
+            {/*Navbar*/}
+            <Navbar fluid rounded className="justify-between bg-[#2c6e49]">
+                <Navbar.Brand className="relative">
+                    <Link href="/">
+                        <svg
+                            className="w-9 h-9 text-[#f9f7ef]"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            viewBox="0 0 24 24">
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="m15 19-7-7 7-7"/>
+                        </svg>
+                    </Link>
+                </Navbar.Brand>
 
-        <div className="flex-1 flex items-center mr-10 justify-center">
-            <Navbar.Brand>
+                <div className="flex-1 flex items-center mr-7 justify-center">
+                    <Navbar.Brand>
             <span className="self-center whitespace-nowrap text-2xl text-[#f9f7ef] font-semibold">
-              Edit Profile
+              Settings
             </span>
-            </Navbar.Brand>
-        </div>
-        </Navbar>
+                    </Navbar.Brand>
+                </div>
+            </Navbar>
 
-        {/* Profile Picture Section */}
+            {/* Profile Picture Section */}
             <div className="flex flex-col items-center justify-center bg-[#f9f7ef]">
                 <div className="flex flex-col items-center mx-14 my-10">
                     <img
                         src={profilePic}
                         alt="Profile"
-                        className="w-40 h-40 rounded-full border-2 border-gray-300"/>
+                        className="w-40 h-40 rounded-full border-2 border-[#f9f7ef]"/>
                     <div>
                         <Button
                             className="text-sm mt-12 bg-[#F09999] border-lg border-[#f7b8b7] enabled:hover:bg-[#f7b8b7] focus:ring-2 focus:ring-[#f7b8b7]"
@@ -84,15 +86,15 @@ export default function Settings() {
 
             {/* Picture Selection Modal */}
             <Modal show={showModal} onClose={() => setShowModal(false)}>
-                <Modal.Header>Select a Profile Picture</Modal.Header>
-                <Modal.Body>
-                    <div className="grid grid-cols-5 gap-4">
+                <Modal.Header className="bg-[#f9f7ef] text-[#2c6e49]">Select a Profile Picture</Modal.Header>
+                <Modal.Body className="bg-[#f9f7ef] text-[#2c6e49]">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 w-full justify-items-center bg-[#f9f7ef]">
                         {pictures.map((url, index) => (
                             <img
                                 key={index}
                                 src={url}
                                 alt={`Profile option ${index + 1}`}
-                                className="w-20 h-20 cursor-pointer border-2 rounded-full"
+                                className="w-20 h-20 cursor-pointer border-2 border-[#f9f7ef] rounded-full"
                                 onClick={() => handlePictureSelect(url)}
                             />
                         ))}
@@ -101,26 +103,58 @@ export default function Settings() {
             </Modal>
 
             {/* Form Section */}
-            <div className="flex max-w-xl flex-col mx-auto gap-7">
+            <div className="bg-[#f9f7ef]">
+            <div className="flex max-w-xl flex-col mx-auto gap-7 bg-[#f9f7ef]">
                 <div>
                     <div className="mb-2 block">
-                        <Label htmlFor="small" className="text-md" value="First Name" />
+                        <Label htmlFor="small" className="text-md" value="First Name"/>
                     </div>
                     <textarea
                         id="small"
-                        className="p-2 border rounded-lg w-full h-11 bg-gray-50 border-gray-300 resize-none overflow-hidden"
-                        placeholder="Enter first name here.."
+                        className="p-2 border border-[#f9f7ef] bg-white rounded-lg w-full h-11 resize-none overflow-hidden"
+                        placeholder="Enter first name here.."/>
+                </div>
+
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="small" className="text-md" value="Last Name"/>
+                    </div>
+                    <textarea
+                        id="small"
+                        className="p-2 border-[#f9f7ef] bg-white rounded-lg w-full h-11 resize-none overflow-hidden"
+                        placeholder="Enter last name here.."/>
+                </div>
+
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="small" className="text-md" value="Email"/>
+                    </div>
+                    <textarea
+                        id="small"
+                        className="p-2 border rounded-lg w-full h-11 bg-white border-[#f9f7ef] resize-none overflow-hidden"
+                        placeholder="Email place holder..."
                     />
                 </div>
 
                 <div>
                     <div className="mb-2 block">
-                        <Label htmlFor="small" className="text-md" value="Last Name" />
+                        <Label htmlFor="small" className="text-md" value=" New Password*"/>
                     </div>
                     <textarea
                         id="small"
-                        className="p-2 border rounded-lg w-full h-11 bg-gray-50 border-gray-300 resize-none overflow-hidden"
-                        placeholder="Enter last name here.."
+                        className="p-2 border rounded-lg w-full h-11 bg-white border-[#f9f7ef] resize-none overflow-hidden"
+                        placeholder="..............."
+                    />
+                </div>
+
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="small" className="text-md" value=" Confirm Password*"/>
+                    </div>
+                    <textarea
+                        id="small"
+                        className="p-2 border rounded-lg w-full h-11 bg-white border-[#f9f7ef] resize-none overflow-hidden"
+                        placeholder="..............."
                     />
                 </div>
 
@@ -133,18 +167,31 @@ export default function Settings() {
                     </div>
                     <textarea
                         id="large"
-                        className="p-4 mb-4 rounded-lg border border-gray-300 w-full h-32 bg-gray-50 resize-none overflow-hidden"
+                        className="p-4 mb-4 rounded-lg border w-full h-32 bg-white border-[#f9f7ef] resize-none overflow-hidden"
                         placeholder="Enter your goals here..."/>
                 </div>
             </div>
+            </div>
+
 
             {/* Save Changes Button */}
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center bg-[#f9f7ef]">
                 <Button
-                    className="text-sm flex flex-col items-center bg-[#F09999] enabled:hover:bg-[#f7b8b7] focus:ring-2 focus:ring-[#f7b8b7] mx-14 my-10"
+                    className="text-sm flex flex-col items-center bg-[#F09999] enabled:hover:bg-[#f7b8b7] focus:ring-2 focus:ring-[#f7b8b7] mx-14 mt-10 mb-7"
                     aria-label="Save Changes">
                     Save Changes
                 </Button>
+            </div>
+
+            {/* Log Out Button */}
+            <div className="flex flex-col items-center justify-center bg-[#f9f7ef]">
+                <Link href="/login">
+                <Button
+                    className="text-sm flex flex-col items-center bg-[#DAA520] enabled:hover:bg-[#E5C062] focus:ring-2 focus:ring-[#E5C062] mx-14 mt-10 mb-14"
+                    aria-label="Log Out">
+                    Log Out
+                </Button>
+                </Link>
             </div>
         </>
     );
