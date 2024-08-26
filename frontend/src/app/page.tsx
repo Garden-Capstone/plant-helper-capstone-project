ad5a0671ef6250e774d01157dfc655d6a531378a
hudsonrow <hrow@cnm.edu>
2024-08-25 22:04:18 -0600

    added nav bar, user plant card, and welcome back message

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,48 @@
 "use client"
-import { Navbar } from "flowbite-react"
 
-import Link from "next/link";
+import PlantNavBar from "@/components/navigation";
+import {UserPlant} from "@/components/userplant";
+
 export default function Home() {
+
+const plantdata = [
+    {
+        image: 'https://images.unsplash.com/photo-1669392597221-bbfd4b6e13ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHJ1YmJlciUyMHBsYW50fGVufDB8fDB8fHww',
+        alt: 'picture of a Rubber Plant',
+        name: 'Rubber Plant'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1640610724404-4214cb70778b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBvdGhvcyUyMHBsYW50fGVufDB8fDB8fHww',
+        alt: 'picture of a Pothos plant',
+        name: 'Pothos'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1616961065849-edf307a08bcb?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        alt: 'Picture of a snake plant',
+        name: 'Snake Plant'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1692896315703-f700fd67c921?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8UGhpbG9kZW5kcm9ufGVufDB8fDB8fHww',
+        alt: 'Picture of a philodendron plant',
+        name: 'Philodendron'
+    }
+]
+
     return (
-
-            <Navbar fluid rounded>
-                <Navbar.Brand as={Link} href="https://flowbite-react.com">
-
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                    <Navbar.Link href="#" active>
-                        Home
-                    </Navbar.Link>
-                    <Navbar.Link as={Link} href="#">
-                        About
-                    </Navbar.Link>
-                    <Navbar.Link href="#">Services</Navbar.Link>
-                    <Navbar.Link href="#">Pricing</Navbar.Link>
-                    <Navbar.Link href="#">Contact</Navbar.Link>
-                </Navbar.Collapse>
-            </Navbar>
-
-)
+        <>
+            <PlantNavBar/>
+            <section className='text-3xl flex flex-row p-6 gap-4 bg-[#3CB371] max-w-[18rem] mx-auto md:mx-14 mt-4 mb-4 border-green-600 border-2 rounded-2xl text-white'>
+                <h1 className='flex justify-center flex-wrap'>
+                    Welcome Back <span className='flex'>(username)!</span>
+                </h1>
+            </section>
+            <div className='justify-center gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                <UserPlant userplant={plantdata[0]}/>
+                <UserPlant userplant={plantdata[1]}/>
+                <UserPlant userplant={plantdata[2]}/>
+                <UserPlant userplant={plantdata[3]}/>
+            </div>
+        </>
+    )
 }
 
---
+++ frontend/src/components/userplant.tsx
@@ -0,0 +1,28 @@
+
+
+type Props = {
+    userplant: {
+        image: string,
+        alt: string,
+        name: string
+    }
+}
+
+export function UserPlant(props:Props) {
+
+
+    return (
+        <>
+            <a href='/' className=''>
+                <section className='border-0 max-w-sm grid grid-row-2 mx-4 shadow-md p-5 rounded-2xl border-black'>
+                    <div className='rounded-lg '>
+                            <img className='rounded-2xl h-96 w-full' src={props.userplant.image} alt={props.userplant.alt}/>
+                    </div>
+                    <div className='text-2xl font-bold text-center pt-3'>
+                        <h5>{props.userplant.name}</h5>
+                    </div>
+                </section>
+            </a>
+        </>
+    )
+}
