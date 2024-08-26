7cddad7a1f3b9766c6f1c3638a1e6d66d4fed000
hudsonrow <hrow@cnm.edu>
2024-08-26 08:09:19 -0600

    replaced placeholder image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 
     const plantimages = [
         {
-            image : 'https://placehold.co/500x500',
-            alt : 'Spider plant placeholder image'
+            image : 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6?q=80&w=1673&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            alt : 'Rubber Plant placeholder image'
         }
     ]
 
