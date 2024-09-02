738374cf16d227db993c6cd132ebdd0cc86e3b75
Ashley <amedrano19@cnm.edu>
2024-09-02 15:24:55 -0600

    added font and logo to title

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react"
+import {PiPottedPlantBold} from "react-icons/pi";
 
 export default function Login() {
     return (
@@ -15,32 +16,33 @@ export default function Login() {
             ></div>
 
             <Card className="flex max-w-lg mx-auto mt-20 overflow-hidden bg-white bg-opacity-90 shadow-2xl">
-                <div>
-                    <p className="flex justify-center pt-16 text-[#2E8B57] text-4xl">Plant Whisperer</p>
+                <div className="flex justify-center pt-16 text-[#2E8B57] text-5xl font-serif">
+                    <PiPottedPlantBold className="text-[#2E8B57]"/>
+                    <p>Plant Whisperer</p>
                 </div>
                 <div>
-                    <p className="flex justify-center pb-14 text-[#3CB371]">Blah Blah Blah</p>
+                    <p className="flex justify-center pb-14 text-[#2E8B57] font-bold">Replanting Your Garden Goals? Log In!</p>
                 </div>
                 <form className="flex flex-col gap-4 items-center">
                     <div>
                         <div className="mb-2 block">
-                            <Label htmlFor="email1" value="Email" className="text-[#2E8B57]"/>
+                            <Label htmlFor="email1" value="Email" className="text-[#2E8B57] text-md font-bold"/>
                         </div>
                         <TextInput className="w-96" id="email1" type="email" placeholder="email" required/>
                     </div>
                     <div>
                         <div className="mb-2 block">
-                            <Label htmlFor="password1" value="Password" className="text-[#2E8B57]"/>
+                            <Label htmlFor="password1" value="Password" className="text-[#2E8B57] text-md font-bold"/>
                         </div>
                         <TextInput className="w-96" id="password1" type="password" placeholder="password" required/>
                     </div>
                     <Button className="w-60 mt-10 py-2 bg-[#2E8B57] enabled:hover:bg-[#2c6e49]" type="submit">
                         Sprout Back In!
                     </Button>
-                    <div className="mt-7 mb-8 text-xs">
+                    <div className="mt-7 mb-8 text-sm">
                         <span>Don't have an account? </span>
-                        <span className="text-[#FF9999]">Sign up </span>
-                        <span>to start growing today!</span>
+                        <a href="/" className="text-[#FF9999] hover:text-[#f7b8b7] font-bold">Sign up </a>
+                        <span>and Dig In!</span>
                     </div>
                 </form>
             </Card>
