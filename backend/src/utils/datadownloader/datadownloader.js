d169820f02dab0172b84b5442f2fbdcac1a26f65
hudsonrow <hrow@cnm.edu>
2024-09-07 11:20:11 -0600

    quick fix

--- backend/src/utils/datadownloader/datadownloader.js
+++ backend/src/utils/datadownloader/datadownloader.js
@@ -2,7 +2,7 @@ async function dataDownloader() {
 
     let page = 1
     do {
-    let data = await fetch(`https://perenual.com/api/species-list?key=sk-zNhW66db33e089a946757&indoor=1&page=${page}`).then(res => res.json())
+    let data = await fetch(`https://perenual.com/api/species-list?key=none&indoor=1&page=${page}`).then(res => res.json())
         // let data = await fetch(`https://perenual.com/api/species-list?key=${process.env.PERENUAL_API_KEY}&indoor=1&page=${page}`)
 
         page++
