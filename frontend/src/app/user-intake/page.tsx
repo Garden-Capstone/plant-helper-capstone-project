e1063c530888bc875c625f6404ecc7c1cd3606bf
Leah Abraham <leahabraham1111@gmail.com>
2024-09-11 13:40:37 -0600

    refactored a whole bunch of files

--- frontend/src/app/user-intake/page.tsx
+++ frontend/src/app/user-intake/page.tsx
@@ -1,54 +1,160 @@
-"use client"
-
-import React from "react";
+'use client'
 import {z} from "zod";
+import {ProfileSchema} from "@/utils/models/profile/profile.validator";
+import {Formik, FormikHelpers, FormikProps} from "formik";
+import {toFormikValidationSchema} from "zod-formik-adapter";
 import {Button, Label, TextInput} from "flowbite-react";
-// import{DisplayError} from "@/app/components/DisplayError";
-// import{DisplayStatus} from "@/app/components/DisplayStatus";
-import Link from "next/link";
-import {Formik} from "formik";
-import {useRouter} from "next/navigation";
+import React from "react";
+import {DisplayError} from "@/app/components/DisplayError";
+import {FormDebugger} from "@/app/components/FormDebugger";
+import {DisplayStatus} from "@/app/components/navigation/DisplayStatus";
 
 
-const formSchema= z.object ({
-    profileUsername: z.string({required_error:"Profile username is required", invalid_type_error:"Please provide a valid username"})
-        .trim()
-        .min(1,{message:"Please provide a valid username (min 1 character long)"})
-        .max(32, {message:"Please provide a valid username (max 32 character long)"}),
+const SignUpSchema = ProfileSchema
+    .omit({profileId: true, profileImageUrl: true, profileGoal: true})
+    .extend({
+        profileEmail: z.string({
+            required_error: 'profileEmail is required',
+            invalid_type_error: 'please provide a valid profileEmail'
+        })
+            .email({ message: 'please provide a valid email'})
+            .max(255, { message: 'profileEmail is too long'}),
+    profilePasswordConfirm: z.string({required_error: 'password confirmation is required'})
+        .min(8, { message: 'please provide a valid password (min 8 characters)'})
+        .max(32, { message: 'please provide a valid password (max 32 characters)'}),
+        profilePassword: z.string({required_error: 'password is required'})
+            .min(8, { message: 'please provide a valid password (min 8 characters)'})
+            .max(32, { message: 'please provide a valid password (max 32 characters)'
+            })
 
+}).refine(data => data.profilePassword === data.profilePasswordConfirm, {
+        message: 'passwords do not match'
+    })
+type SignUp = z.infer< typeof SignUpSchema>
+export default function Page() {
+            const initialValues = {
+                profileEmail: '',
+                profileUserName: '',
+                profilePassword: '',
+                profilePasswordConfirm: ''
+            }
+            /*
+            *
+            *
+             */
 
-    profileEmail: z.string({required_error:"Please provide a valid email to sign in", invalid_type_error:"Please provide a valid email to sign in"})
-        .email("Please provide a valid email to sign in")
-        .max(255, {message:"Please provide a valid email with a max length of 255"}),
-
-
-        profileGoal: z.string({required_error:"Please select a goal from the drop down menu", invalid_type_error:"Please select a goal form the drop down menu"}),
-
-    profilePassword: z.string({required_error:"Please provide a valid password to sign in", invalid_type_error:"Please provide a valid password to sign in"})
-    .min(8, {message:"Please provide a password with a minimum 8 characters"})
-    .max(32, {message:"Please provide a password with a maximum 32 characters"}),
-
-
-        profilePasswordConfirm: z.string({required_error:"Please confirm your password, passwords must match", invalid_type_error:"Please confirm your password, passwords must match"})
-            .min(8, {message:"Please provide a password with a minimum 8 characters"})
-            .max(32, {message:"Please provide a password with a maximum 32 characters"}),
-
+    const handleSubmit = (values: SignUp, actions: FormikHelpers<SignUp>) => {
+        const {setStatus, resetForm} = actions
+        fetch('/apis/sign-up', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+            .then(response => response.json())
+            .then(data => {
+                const type = 'failure'
+                setStatus({type, message: data.message})
+                if (data.status === 200) {
+                    resetForm()
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                setStatus({type: 'failure', message: 'An error occurred try again later'})
+            })
     }
-)
-.refine(data=> data.profilePassword === data.profilePasswordConfirm, {message:"passwords do not match"})
-
-
-type FormSchema = z.infer<typeof formSchema>
-
-export default function form (){
-    const initialValues = {
-        profileEmail: '',
-        profilePassword: '',
-        profileGoal: '',
-        profilePasswordConfirm: '',
-        profileUsername: '',
-        profileImage: ''
-    }
-
+    return(
+        <>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={toFormikValidationSchema(SignUpSchema)}>
+            {SignUpFormContent}
+            </Formik>
+        </>
+    )
 }
+export function SignUpFormContent(props: FormikProps<SignUp>) {
+   const {
+       status,
+       values,
+       errors,
+       touched,
+       handleChange,
+       handleBlur,
+       handleSubmit,
+       handleReset
+   }  = props;
 
+   return (
+       <>
+           <div className="container mx-auto">
+           <form onSubmit={handleSubmit} className="flex min-h-auto gap-4 min-w-full flex-col grow">
+               <div>
+                       <div className="mb-2 block">
+                        <label htmlFor="email1"/>
+                       </div>
+
+                   <TextInput
+                       onChange={handleChange}
+                       onBlur={handleBlur}
+                       autoComplete='email'
+                       id="email1"
+                       name={'profileEmail'}
+                       type="email"
+                       value={values.profileEmail}
+                    />
+                     <DisplayError errors={errors} touched={touched} field={'profileEmail'}/>
+               </div>
+               <div>
+                    <div className="md-2 block">
+                         <Label htmlFor="profileName" value="name"/>
+                    </div>
+                    <TextInput
+                         onChange={handleChange}
+                         onBlur={handleBlur}
+                         autoComplete='username'
+                         id="profileName"
+                         name={'profileName'}
+                         type="text"
+                         value={values.profileUserName}
+                     />
+                    <DisplayError errors={errors} touched={touched} field={'profileName'}/>
+               </div>
+               <div>
+                    <div className="mb-2 block">
+                            <Label htmlFor="profilePassword" value="password"/>
+                    </div>
+                     <TextInput
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            autoComplete={'new-password'}
+                            name='profilePassword'
+                            id="profilePassword"
+                            type="password"
+                            value={values.profilePassword}
+                    />
+                    <DisplayError errors={errors} touched={touched} field={'profilePassword'}/>
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                            <Label htmlFor="profilePasswordConfirm" value="password confirm"/>
+                    </div>
+                    <TextInput
+                        value={values.profilePasswordConfirm}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        id="profilePasswordConfirm"
+                        name="profilePasswordConfim=rm"
+                        autoComplete={'new-password confirm'} type="password"
+                    />
+                    <DisplayError errors={errors} touched={touched} field={'profilePasswordConfirm'}/>
+                </div>
+               <Button color={'success'} type="submit">Submit</Button>
+               <Button color={'failure'} type="reset" onClick={handleReset}>Reset</Button>
+               <DisplayStatus status={status}/>
+           </form>
+                    <FormDebugger {...props} />
+           </div>
+       </>
+   )
+}
---
+++ frontend/src/utils/models/profile/profile.validator.ts
@@ -0,0 +1,28 @@
+import {z} from 'zod'
+
+export const ProfileSchema = z.object({
+    profileId: z.string({
+        required_error: 'profileId is required',
+        invalid_type_error: 'Please provide a valid profileId'
+    })
+        .uuid({ message: 'please provide a valid profileId'}),
+    profileGoal: z.string({
+        required_error: 'profile about is a required field.',
+        invalid_type_error: 'please provide a valid profile about'
+    })
+        .max(255, { message: 'profile about length is too long'})
+        .nullable(),
+    profileImageUrl: z.string({
+        required_error: 'profileImage is required',
+        invalid_type_error: 'please provide a valid profileImageUrl'
+    })
+        .trim()
+        .url({ message: 'please provide a valid profile image url'})
+        .max(255, { message: 'profile image url is too long'})
+        .nullable(),
+profileUserName: z.string ({required_error: 'profile user name is required', invalid_type_error: 'please provide a valid profile user name'})
+    .trim()
+    .min(1, { message: 'please provide a valid profile user name (min 1 characters)'})
+    .max(32, { message: 'please provide a valid profile user name (max 32 characters)'})
+})
+export type Profile = z.infer<typeof ProfileSchema>
