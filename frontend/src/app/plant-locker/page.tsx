144468548e32228984ef67497e0084096a7ddeb4
hudsonrow <hrow@cnm.edu>
2024-09-18 16:19:28 -0600

    added styling for the plant locker

--- frontend/src/app/plant-locker/page.tsx
+++ frontend/src/app/plant-locker/page.tsx
@@ -9,6 +9,7 @@ import {SearchDetailsNav} from "@/app/components/navigation/SearchDetailsNav";
 import {Plant} from "@/utils/models/plant/plant.validator";
 import {fetchPlantByPlantId} from "@/utils/models/plant/plant.model";
 import {PageProps} from "@/utils/interfaces/NextComponent";
+import ClientNav from "@/app/navcomponents/ClientNav";
 
 export default async function PlantLocker() {
 
@@ -23,19 +24,21 @@ export default async function PlantLocker() {
 
     return (
         <>
-            <SearchDetailsNav/>
-            <section className='flex font-serif text-5xl pb-6 pt-6 gap-4 max-w-full
-            mx-auto md:mx-14 mt-4 mb-4 border-green-600 text-[#2C6E49]'>
-                <h1 className=' justify-center '>
-                    Welcome Back <span className='text-[#3CB371]'>{session.profile.profileUsername}!</span>
-                </h1>
-            </section>
-            <div className='text-5xl bg-[#2C6E49] font-bold py-3 font-serif text-center' >
-                <h5 className='text-white'>Plant Locker</h5>
-            </div>
-            <div className='container py-10 max-w-full mx-6 justify-center gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-                {lockers.map(locker => <UserPlantCard key={locker.lockerProfileId} locker={locker}/>)}
-            </div>
+            <ClientNav/>
+                <div className='bg-[#F9F7EF]'>
+                    <section className='flex font-serif text-5xl pb-6 pt-6 gap-4 max-w-full
+                    mx-auto md:mx-14 mt-4 mb-4 border-green-600 text-[#2C6E49] '>
+                        <h1 className=' justify-center '>
+                            Welcome Back <span className='text-[#3CB371]'>{session.profile.profileUsername}!</span>
+                        </h1>
+                    </section>
+                    <div className='text-5xl bg-[#2C6E49] font-bold py-3 font-serif text-center' >
+                        <h5 className='text-white'>Plant Locker</h5>
+                    </div>
+                    <div className='container py-10 max-w-full mx-6 justify-center gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                        {lockers.map(locker => <UserPlantCard key={locker.lockerProfileId} locker={locker}/>)}
+                    </div>
+                </div>
         </>
     )
 }
