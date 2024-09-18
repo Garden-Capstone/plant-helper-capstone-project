faac76ee7a077496827017bfc2d320d94b6072ad
Leah Abraham <leahabraham1111@gmail.com>
2024-09-18 09:44:32 -0600

    made questions form/formik

--- frontend/src/app/questions/questionnaireForm.tsx
+++ frontend/src/app/questions/questionnaireForm.tsx
@@ -1,13 +1,18 @@
 import {z} from "zod";
-import {FormikProps} from "formik";
-import {Button, Label, TextInput} from "flowbite-react";
+import {FormikProps, Formik, FormikHelpers, Field} from "formik";
+import {Button, Label, Radio, Select, TextInput} from "flowbite-react";
 import {DisplayStatus} from "@/app/components/navigation/DisplayStatus";
+import {PlantSchema} from "@/utils/models/plant/plant.validator";
+import React from "react";
+import {DisplayError} from "@/app/components/DisplayError";
+import {toFormikValidationSchema} from "zod-formik-adapter";
 
 
-const PlantSchema = PlantSchema
+
+const FormSchema = PlantSchema
     .omit({plantId: true, plantName: true, plantSpecies: true, plantDescription: true, plantImageUrl: true, plantToxicity: true, plantPropagation: true, plantMaintenance: true})
     .extend({
-       plantWatering: z.string({
+        plantWatering: z.string({
             required_error: 'selection is required',
             invalid_type_error: ' please select a valid option'
         }),
@@ -18,12 +23,13 @@ const PlantSchema = PlantSchema
         }),
         plantGrowthRate: z.string({
             required_error: 'selection is required',
-            invalid_type_error: ' please select a valid option'
+            invalid_type_error: ' please select a valid option',
         }),
+    })
+type Form = z.infer <typeof FormSchema>;
 
 
 
-type Questions = z.infer< typeof PlantSchema>
 export function QuestionnaireForm() {
 
     const initialValues = {
@@ -37,8 +43,8 @@ export function QuestionnaireForm() {
      */
 
 
-    const handleSubmit = (values: Questions, actions: FormikHelpers<Questions> ) => {
-        const {setStatus, resetForm} = actions
+    const handleSubmit = (values: Form, actions: FormikHelpers<Form> ) => {
+        const {setStatus,resetForm} = actions
         fetch('/apis/questionnaireForm', {
             method: 'POST',
             headers: {
@@ -63,9 +69,9 @@ export function QuestionnaireForm() {
 
     return(
         <>
-            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={toFormikValidationSchema(PlantSchema)}>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={toFormikValidationSchema(FormSchema)}>
 
-                {questionnaireForm}
+                {QuestionnaireFormContent}
 
             </Formik>
         </>
@@ -73,7 +79,7 @@ export function QuestionnaireForm() {
 }
 
 
-export function QuestionnaireForm(props: FormikProps<Questions>) {
+export function QuestionnaireFormContent(props:any) {
 
     const {
         status,
@@ -91,23 +97,116 @@ export function QuestionnaireForm(props: FormikProps<Questions>) {
         <>
             <form onSubmit={handleSubmit} className="flex min-h-auto gap-4 min-w-full flex-col grow">
                 <div>
-                    <div className="mb-2 block">
-                        <Label htmlFor="email1" value="email"/>
-                    </div>
-                    <TextInput
-                        onChange={handleChange}
-                        onBlur={handleBlur}
+                    <fieldset className="flex max-w-md flex-col gap-4">
+                        <legend className="mb-4">
+                            Question 1: Select the option that best describes the type of sunlight your plant will
+                            receive.
+                        </legend>
+                        <div className="gap-2">
+                            <Select
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                name={'plantSunlight'}
+                            >
+                                <option value={''}>
 
-                    />
-                    <DisplayError errors={errors} touched={touched} />
+                                </option>
+
+                                <option value={'full sun'}>
+                                    full sun
+                                </option>
+
+                                <option value={'part sun'}>
+                                    part sun
+                                </option>
+
+                                <option value={'part shade'}>
+                                    part shade
+                                </option>
+
+                                <option value={'filtered shade'}>
+                                    filtered shade
+                                </option>
+                            </Select>
+                            <DisplayError field={'plantSunlight'} errors={errors} touched={touched}/>
+                        </div>
+                    </fieldset>
+
+
+                    <fieldset className="flex max-w-md flex-col gap-4">
+                        <legend className="mb-4">
+                            Question 2: Tell us about your availability for watering your plant.
+                        </legend>
+                        <div className="gap-2">
+                            <Select
+
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                name={'plantWatering'}
+                            >
+                                <option value={''}>
+
+                                </option>
+
+                                <option value={'Frequent'}>
+                                    Frequent
+                                </option>
+
+                                <option value={'Average'}>
+                                    Average
+                                </option>
+
+                                <option value={'Minimum'}>
+                                    Minimum
+                                </option>
+
+                            </Select>
+                            <DisplayError field={'plantWatering'} errors={errors} touched={touched}/>
+                        </div>
+                    </fieldset>
+
+                    <fieldset className="flex max-w-md flex-col gap-4">
+                        <legend className="mb-4">
+                            Question 3: What is the amount of space your plant will have to grow in?
+                        </legend>
+                        <div className="gap-2">
+                            <Select
+
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                name={'plantGrowthRate'}
+                            >
+                                <option value={''}>
+
+                                </option>
+
+                                <option value={'Low'}>
+                                    Low
+                                </option>
+
+                                <option value={'High'}>
+                                    High
+                                </option>
+
+                                <option value={'Moderate'}>
+                                    Moderate
+                                </option>
+
+                            </Select>
+                            <DisplayError field={'plantGrowthRate'} errors={errors} touched={touched}/>
+
+                        </div>
+                    </fieldset>
+
                 </div>
 
-                <Button color={'success'} type="submit">Submit</Button>
-                <Button  color={'failure'} type="reset" onClick={handleReset}>Reset</Button>
+                <Button color={'pink'} type="submit">Submit</Button>
+                <Button color={'green'} type="reset" onClick={handleReset}>Reset</Button>
                 <DisplayStatus status={status}/>
             </form>
-            <FormDebugger {...props} />
+
         </>
 
     )
 }
+
