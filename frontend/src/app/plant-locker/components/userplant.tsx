9b6e02cd3549b08ec2f0c25532fbe647c739123b
hudsonrow <hrow@cnm.edu>
2024-09-18 14:34:09 -0600

    added functionality for plant locker to direct to the user plant page

--- frontend/src/app/plant-locker/components/userplant.tsx
+++ frontend/src/app/plant-locker/components/userplant.tsx
@@ -1,5 +1,5 @@
 
-import {Plant} from "@/utils/models/plant/plant.validator";
+
 import {Locker} from "@/utils/models/locker/locker.validator";
 import {fetchPlantByPlantId} from "@/utils/models/plant/plant.model";
 
@@ -13,19 +13,19 @@ export async function UserPlantCard(props: UserPlantCardProps) {
 
     const {lockerId, lockerProfileId, lockerImageUrl, lockerName} = locker
     const plant = await fetchPlantByPlantId(locker.lockerPlantId)
-    console.log(plant)
+
     return (
         <>
-            {/*<a href={`/user-plant/${plantId}`}>*/}
-                <section className='border-0 max-w-sm grid grid-row-2 mx-4 shadow-md p-5 rounded-2xl border-black'>
-                    <div className='rounded-lg '>
-                            <img className='rounded-2xl h-96 w-full' src={plant.plantImageUrl} alt={plant.plantName} />
+            <a href={`/user-plant/${plant.plantId}`} className=''>
+                <section className='border-0 max-w-sm grid grid-row-2 mx-4 shadow-lg p-3 bg-white rounded-2xl border-black'>
+                    <div className='rounded-lg'>
+                            <img className='rounded-2xl h-80 w-full' src={plant.plantImageUrl} alt={plant.plantName} />
                     </div>
-                    <div className='capitalize text-2xl font-bold text-center pt-3'>
+                    <div className='font-serif text-[#2C6E49] capitalize text-3xl font-bold text-center pt-3'>
                         <h5>{plant.plantName}</h5>
                     </div>
                 </section>
-            {/*</a>*/}
+            </a>
         </>
     )
 }
---
+++ frontend/src/app/user-plant/[plantId]/page.tsx
@@ -0,0 +1,62 @@
+
+
+import {SearchDetailsNav} from "@/app/components/navigation/SearchDetailsNav";
+import {fetchPlantByPlantId} from "@/utils/models/plant/plant.model";
+import {PageProps} from "@/utils/interfaces/NextComponent";
+
+export default async function UserPlant(props: PageProps<{plantId:string}>) {
+
+    const plantId = props.params.plantId
+    const plant = await fetchPlantByPlantId(plantId)
+
+    return (
+        <>
+            <SearchDetailsNav/>
+
+            <section className='p-12 float-left'>
+                <img className='max-h-96' src={plant.plantImageUrl} alt={plant.plantName}/>
+            </section>
+
+            <div className="bg-[#F9F7EF]">
+                <div className="capitalize text-2xl md:text-7xl mt-12 ml-14 text-[#2C6E49] bg-[#F9F7EF]">
+                    <h2>{plant.plantName}</h2>
+                    <div className="capitalize text-xl md:text-4xl md:ml-14 text-[#E5C062]">
+                        <span>{plant.plantSpecies}</span>
+                    </div>
+                </div>
+                <div className="mx-14 mt-9 md:text-lg text-sm bg-[#F9F7EF] text-[#2C6E49]">
+                    <span>{plant.plantDescription}</span>
+                </div>
+            </div>
+            {/*care instruction*/}
+            <div className="font-lg grid grid-cols-1 md:grid-cols-4 mt-10">
+                <div className="mt-4 mx-10">
+                    <h3 className="text-2xl font-semibold mb-2 text-[#2C6E49]">Watering</h3>
+                    <p className="text-sm mb-3 text-[#2E8B57]">
+                        {plant.plantWatering}
+                    </p>
+                </div>
+                <div className="mt-4 mx-10">
+                    <h3 className="text-2xl font-semibold mb-2 text-[#2C6E49]">Sunlight</h3>
+                    <p className="text-sm mb-3 text-[#2E8B57]">
+                        {plant.plantSunlight}
+                    </p>
+                </div>
+                <div className="mt-4 mx-10">
+                    <h3 className="text-2xl font-semibold mb-2 text-[#2C6E49]">Growth Rate</h3>
+                    <p className="text-sm mb-3 text-[#2E8B57]">
+                        {plant.plantGrowthRate}
+                    </p>
+                </div>
+                <div className="mt-4 mx-10">
+                    <h3 className="text-2xl font-semibold mb-2 text-[#2C6E49]">Toxicity</h3>
+                    <p className="text-sm mb-3 text-[#2E8B57]">
+                        {plant.plantToxicity}
+                    </p>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
